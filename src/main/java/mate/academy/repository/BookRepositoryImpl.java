50638d886a8b5a7ac564e9d2635add6f1db3cb6c
StoneBlood <brkalix@gmail.com>
2024-11-25 21:22:25 +0200

    created the BookRepositoryImpl class and implemented the methods save and findAll

---
+++ src/main/java/mate/academy/repository/BookRepositoryImpl.java
@@ -0,0 +1,58 @@
+package mate.academy.repository;
+
+import mate.academy.model.Book;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class BookRepositoryImpl implements BookRepository {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public BookRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Book save(Book book) {
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(book);
+            transaction.commit();
+            return book;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can not add book: " + book);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Book> findAll() {
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            return session.createQuery("from Book", Book.class).list();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to get all book", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
