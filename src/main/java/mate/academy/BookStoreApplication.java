ecfa199cc12861b62ba4b5de274ae34bf0431d8d
StoneBlood <brkalix@gmail.com>
2024-11-26 14:08:16 +0200

    used RequiredArgsConstructor

--- src/main/java/mate/academy/BookStoreApplication.java
+++ src/main/java/mate/academy/BookStoreApplication.java
@@ -1,11 +1,14 @@
 package mate.academy;
 
+import mate.academy.model.Book;
 import mate.academy.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class BookStoreApplication {
@@ -21,6 +24,14 @@ public class BookStoreApplication {
         return new CommandLineRunner() {
             @Override
             public void run(String... args) throws Exception {
+                Book book = new Book();
+                book.setTitle("The Headless Horseman");
+                book.setAuthor("Thomas Mayne Reid");
+                book.setPrice(BigDecimal.valueOf(200));
+                book.setIsbn("123-234-345-456");
+
+                bookService.save(book);
+                System.out.println(book);
             }
         };
     }
--- src/main/java/mate/academy/repository/BookRepositoryImpl.java
+++ src/main/java/mate/academy/repository/BookRepositoryImpl.java
@@ -1,21 +1,18 @@
 package mate.academy.repository;
 
 import java.util.List;
+
+import lombok.RequiredArgsConstructor;
 import mate.academy.model.Book;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class BookRepositoryImpl implements BookRepository {
     private final SessionFactory sessionFactory;
-
-    @Autowired
-    public BookRepositoryImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
 
     @Override
     public Book save(Book book) {
