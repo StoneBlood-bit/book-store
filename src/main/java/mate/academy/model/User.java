73f5bdaa6e864b52f193f37048c115534c85dbf7
StoneBlood <brkalix@gmail.com>
2024-12-10 23:23:12 +0200

    created the model `User`

---
+++ src/main/java/mate/academy/model/User.java
@@ -0,0 +1,27 @@
+package mate.academy.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    private String shippingAddress;
+}
